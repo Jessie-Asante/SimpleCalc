91e4256832529441298e30029c737f217875ded0
Jessie Asante <kaprikon1995@gmail.com>
2023-04-07 16:09:09 +0000

    gg

--- proj.js
+++ proj.js
@@ -2,21 +2,29 @@
 (function() {
     let screen = document.querySelector('.screen');
     let buttons = document.querySelectorAll('.btn');
-    let equals = document.querySelector('.button-equal');
-    let clears = document.querySelector('.button-clear');
-
-
+    let clears = document.querySelector('.btn-clear');
+    let equals = document.querySelector('.btn-equal');
 
     buttons.forEach(function(button){
        button.addEventListener('click', function(e){
            let value = e.target.dataset.num;
            screen.value += value;
-       });
+       })
     });
 
-    // we arw egayegiua
-
-    equals.addEventListener('click', function(results){
-
+   
+    equals.addEventListener('click', function(e){ 
+        if (screen.value === '') {
+            screen.value = " ";
+        }
+        else{
+            let answer = eval(screen.value);
+            screen.value = answer;
+        }
+        
+    })
+    
+    clears.addEventListener('click', function(e){
+        screen.value ="";
     })
 })();
